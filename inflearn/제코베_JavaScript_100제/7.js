b873cb475ea3041c00f2dd5dc61465f04137a6d1
안서진 <ser0805@gmail.com>
2021-07-05 07:53:46 +0900

    add Inflearn: 제코베 자바스크립트 7번

---
+++ inflearn/제코베_JavaScript_100제/7.js
@@ -0,0 +1,10 @@
+// JavaScript는 문자, 밑줄(_), 달러($)로 시작해야하며,
+// 예약어인 let과 숫자로 시작하는 1age는 변수명이 될 수 없다. 
+var age = 0;
+var Age = 0;
+// var let = 0;
+var _age = 0;
+// var 1age = 0;
+console.log(age);
+console.log(Age);
+console.log(_age);
