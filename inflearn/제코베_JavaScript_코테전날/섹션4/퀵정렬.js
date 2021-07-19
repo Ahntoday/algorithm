6a49815981d79ad8f96b66078493f8b990d88e89
안서진 <ser0805@gmail.com>
2021-07-19 12:16:41 +0900

    Add Inflearn: 제코베 눈코테 섹션4 퀵정렬

---
+++ inflearn/제코베_JavaScript_코테전날/섹션4/퀵정렬.js
@@ -0,0 +1,44 @@
+// 퀵정렬 (worst- O(nlong2n), best - O(n**2))
+let data = [66, 77, 54, 32, 10, 5, 11, 15];
+
+// [66, 77, 54, 32, 10, 5, 11, 15]
+// 피봇값: 66
+// [77, 54, 32, 10, 5, 11, 15] + [66] + [77]
+
+// 피봇값: 54
+// [32, 10, 5, 11, 15] + [54] + [66] + [77]
+
+// 피봇값: 32
+// [10, 5, 11, 15] + [32] + [54] + [66] + [77]
+
+// 피봇값: 10
+// [5] + [10] + [11, 15] + [32] + [54] + [66] + [77]
+
+// 피봇값: 11
+// [5] + [10] + [11] + [15] + [32] + [54] + [66] + [77]
+
+function quickSort(arr) {
+    let arrLen = arr.length;
+
+    if (arrLen <= 1) {
+        return arr;
+    }
+
+    let pivot = [arr.shift()];
+    let group1 = [];
+    let group2 = [];
+
+    for (let i in arr) {
+        if (arr[i] < pivot) {
+            group1.push(arr[i]);
+        } else {
+            group2.push(arr[i]);
+        }
+    }
+
+    console.log(`그룹하나: ${group1}\n 그룹둘: ${group2}\n 피봇값: ${pivot}`);
+
+    return quickSort(group1).concat(pivot, quickSort(group2));
+}
+
+console.log(quickSort(data));
