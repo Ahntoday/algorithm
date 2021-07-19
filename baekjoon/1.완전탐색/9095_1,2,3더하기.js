f98a149661dae8c3520dc99f3cd1a41de4f2c92d
안서진 <ser0805@gmail.com>
2021-07-20 08:25:19 +0900

    Fix BaekJoon: 완전탐색 9095_1,2,3더하기 백준 입출력으로 수정

--- baekjoon/1.완전탐색/9095_1,2,3더하기.js
+++ baekjoon/1.완전탐색/9095_1,2,3더하기.js
@@ -1,6 +1,26 @@
-const T = 3;
-const input = [4, 7, 10];
-const data = [1, 2, 3];
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+rl.on("line", function (line) { //여러줄 입력 
+    input.push(line)
+    //rl.close()가 없어서 계속입력 
+    //로컬에서 입력을 중지할려면 입력을 한 후 'ctrl + D'을 통해 입력 종료 
+}).on("close", function () {
+    // 이런식으로 적절하게 입력값을 처리해줘야한다. 
+    let T = parseInt(input[0]);
+    for (let i = 1; i < T + 1; i++) {
+        console.log(solve(input[i]));
+    }
+    //프로세스 종료 
+    process.exit();
+});
+
+// const T = 3;
+// const input = [4, 7, 10];
 
 function solve(n) {
     if (n == 1) {
@@ -15,6 +35,6 @@ function solve(n) {
     return solve(n - 3) + solve(n - 2) + solve(n - 1)
 }
 
-for (let i = 0; i < T; i++) {
-    console.log(solve(input[i]));
-}
+// for (let i = 0; i < T; i++) {
+//     console.log(solve(input[i]));
+// }
