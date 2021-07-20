a783c979bc1b47aa68ecc35c9e5e96c12bbce2b3
안서진 <ser0805@gmail.com>
2021-07-20 09:59:50 +0900

    Add BaekJoon: 완전탐색 1018_체스판다시칠하기

---
+++ baekjoon/1.완전탐색/1018_체스판다시칠하기.js
@@ -0,0 +1,92 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+// const input = [
+//     '8 8',
+//     'WBWBWBWB',
+//     'BWBWBWBW',
+//     'WBWBWBWB',
+//     'BWBBBWBW',
+//     'WBWBWBWB',
+//     'BWBWBWBW',
+//     'WBWBWBWB',
+//     'BWBWBWBW'
+// ];
+
+// const input = [
+//     '10 13',
+//     'BBBBBBBBWBWBW',
+//     'BBBBBBBBBWBWB',
+//     'BBBBBBBBWBWBW',
+//     'BBBBBBBBBWBWB',
+//     'BBBBBBBBWBWBW',
+//     'BBBBBBBBBWBWB',
+//     'BBBBBBBBWBWBW',
+//     'BBBBBBBBBWBWB',
+//     'WWWWWWWWWWBWB',
+//     'WWWWWWWWWWBWB'
+// ];
+
+const white = [
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW'
+];
+
+const black = [
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB'
+];
+
+function countNotWhite(y, x) { // 행 열
+    let count = 0;
+    for (let i = y; i < y + 8; i++) {
+        for (let j = x; j < x + 8; j++) {
+            if (input[i][j] !== white[i - y][j - x]) {
+                count += 1;
+            }
+        }
+    }
+    return count;
+}
+
+function countNotBlack(y, x) {
+    let count = 0;
+    for (let i = y; i < y + 8; i++) {
+        for (let j = x; j < x + 8; j++) {
+            if (input[i][j] !== black[i - y][j - x]) {
+                count += 1;
+            }
+        }
+    }
+    return count;
+}
+
+let NM = input.shift().split(' ').map(function (n) {
+    return parseInt(n, 10);
+});
+const N = NM.shift();
+const M = NM.shift();
+
+let result = [];
+for (let i = 0; i + 7 < N; i++) {
+    for (let j = 0; j + 7 < M; j++) {
+        result.push(countNotWhite(i, j));
+        result.push(countNotBlack(i, j));
+    }
+}
+
+// console.log(result);
+
+console.log(Math.min.apply(null, result));
