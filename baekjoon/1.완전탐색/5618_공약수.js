4641954d30c344623c14bb276d65fcbbdada04b5
안서진 <ser0805@gmail.com>
2021-08-02 08:16:30 +0900

    Add BaekJoon: 완전탐색 5618_공약수

--- baekjoon/1.완전탐색/5618_공약수.js
+++ baekjoon/1.완전탐색/5618_공약수.js
@@ -1,52 +1,29 @@
-// 1. 백준 제출을 위한 입출력 코드
-// const readline = require('readline');
+const fs = require('fs');
+const input = fs.readFileSync("../test.txt").toString().trim().split('\n');
 
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
+const n = parseInt(input.shift(), 10);
+const data = input.toString().split(' ').map(Number);
+data.sort((a, b) => a - b);
 
-// let input = [];
-// let data = [];
-// rl.on('line', function (line) {
-//     input.push(line);
-// }).on('close', function () {
-//     const n = parseInt(input[0]);
-//     const data = input[1].split(' ').map((el) => parseInt(el));
-
-//     solution(n, data);
-
-//     process.exit();
-// });
-
-// 2. 백준 제출을 위한 입출력 코드 (이 문제에서는 안된다고 공지됨)
-// const fs = require('fs');
-// const n = parseInt(fs.readFileSync('/dev/stdin').toString().trim(), 10);
-// const input = fs.readFileSync('/dev/stdin').toString().split('').map(function(n) => {return parseInt(n, 10)});
-
-const n = 2;
-const data = [75, 125];
-let arrays = [];
 let result = [];
 
 function solution(n, data) {
+    let arrays = [];
     for (let i = 0; i < n; i++) {
         let array = new Array();
-
-        for (let j = 0; j < data[i]; j++) {
+        for (let j = 1; j <= data[i]; j++) { // 각각 수의 공약수를 구함
             if (data[i] % j == 0) {
                 array.push(j);
             }
         }
-        // console.log(array);
         arrays.push(array);
     }
-    // console.log(arrays);
-
+    result = arrays[0];
     for (let i = 0; i < arrays.length - 1; i++) {
-        result = arrays[i].filter((it) => arrays[i + 1].includes(it));
+        let temp = arrays[i].filter((it) => arrays[i + 1].includes(it)); // 두 배열에 공통으로 있는 것을 temp에 담음
+        result = result.filter((el) => temp.includes(el)); // temp에 있는 것으로 result를 다시 필터링함
     }
-    console.log(result);
 }
 
 solution(n, data);
+result.forEach((el) => console.log(el));
