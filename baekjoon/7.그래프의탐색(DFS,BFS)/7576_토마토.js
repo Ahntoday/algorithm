8b3de6f284fe56de0bb85d486521bb4670825d17
안서진 <ser0805@gmail.com>
2021-08-30 22:27:16 +0900

    Refactor BaekJoon: 그래프의탐색(BFS) 7576_토마토

--- baekjoon/7.그래프의탐색(DFS,BFS)/7576_토마토.js
+++ baekjoon/7.그래프의탐색(DFS,BFS)/7576_토마토.js
@@ -1,14 +1,19 @@
 const fs = require("fs");
-let tomato = fs.readFileSync("../test.txt").toString().split("\n");
-tomato = tomato.map(el => el.trim().split(" "));
-const row = Number(tomato[0][1]); // 세로: 행
-const column = Number(tomato[0][0]); // 가로: 열
-tomato.shift();
-
+let input = fs.readFileSync("../test.txt").toString().split("\n");
+let MN = input.shift().split(' ').map(el => parseInt(el, 10));
+const column = MN.shift(); // 세로
+const row = MN.shift(); // 가로
+let tomato = Array.from(new Array(row), () => new Array(column).fill(0));
+for (let i = 0; i < row; i++) {
+    const temp = input[i].split(' ').map(el => parseInt(el, 10));
+    tomato[i] = temp; // 익은 토마토 1, 안 익은 토마토 0
+}
 const ripeTomato = Array.from(new Array(row), () => new Array(column).fill(0));
 const moveRow = [0, 0, 1, -1];
 const moveCol = [1, -1, 0, 0];
 let answer;
+const queue = [];
+let zero = false;
 
 const rangeCheck = (r, c) => {
     if (r >= 0 && r < row && c >= 0 && c < column) {
@@ -25,8 +30,8 @@ const BFS = (queue) => {
         for (let n = 0; n < 4; n++) {
             const nextRow = currentRow + moveRow[n];
             const nextCol = currentCol + moveCol[n];
-            if (rangeCheck(nextRow, nextCol) && !ripeTomato[nextRow][nextCol] && Number(tomato[nextRow][nextCol]) === 0) {
-                // 동서남북 중 이동가능하고, 익은 토마토 배열에 안들어가 있고, 원래 안 익은 토마토라면 큐에 추가
+            if (rangeCheck(nextRow, nextCol) && !ripeTomato[nextRow][nextCol] && tomato[nextRow][nextCol] === 0) {
+                // 동서남북 중 이동가능하고, 익은 토마토 배열에 안들어가 있고, 원래 안 익은 토마토(0)라면 큐에 추가
                 queue.push([nextRow, nextCol]);
                 ripeTomato[nextRow][nextCol] = ripeTomato[currentRow][currentCol] + 1;
                 answer = ripeTomato[nextRow][nextCol];
@@ -37,23 +42,20 @@ const BFS = (queue) => {
     answer = answer - 1;
 }
 
-const queue = [];
-let zero = false;
-
 for (let r = 0; r < row; r++) {
     for (let c = 0; c < column; c++) {
-        if (Number(tomato[r][c]) === 1) { // 토마토가 존재한다면 큐에 넣는다.
-            queue.push([Number(r), Number(c)]);
-            ripeTomato[Number(r)][Number(c)] = 1; // 처음부터 익어있으므로 표시
-        } else if (Number(tomato[r][c]) === -1) {
-            ripeTomato[Number(r)][Number(c)] = -1;
-        } else if (Number(tomato[r][c]) === 0) {
+        if (tomato[r][c] === 1) { // 익은 토마토라면 (토마토가 존재한다면) 큐에 넣는다.
+            queue.push([r, c]);
+            ripeTomato[r][c] = 1; // 처음부터 익어있으므로 표시
+        } else if (tomato[r][c] === -1) {
+            ripeTomato[r][c] = -1;
+        } else if (tomato[r][c] === 0) { // 익지않은 토마토
             zero = true;
         }
     }
 }
 
-if (!zero) { // 처음부터 다 익어져 있다면 0 출력해야함. 하나라도 안 익은 토마토가 있으면 zero를 false로 변경
+if (!zero) { // 처음부터 다 익어져 있다면 0 출력해야함. 하나라도 안 익은 토마토가 있으면 zero를 true로 변경
     answer = 0;
 } else {
     BFS(queue);
