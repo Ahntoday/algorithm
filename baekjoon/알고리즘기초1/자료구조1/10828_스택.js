8d92f9494727484754bca212a8dcd09c771a04b5
안서진 <ser0805@gmail.com>
2021-10-10 10:28:05 +0900

    Add BaekJoon: 알고리즘기초1 자료구조1 10828_스택

---
+++ baekjoon/알고리즘기초1/자료구조1/10828_스택.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const input = fs.readFileSync("../../test.txt").toString().trim().split('\n');
+const N = parseInt(input.shift(), 10);
+let stack = [];
+let result = [];
+
+for (let i = 0; i < N; i++) {
+    switch (input[i]) {
+        case 'pop':
+            result.push(stack.pop() || -1);
+            break;
+
+        case 'top':
+            result.push(stack[stack.length - 1] || -1);
+            break;
+
+        case 'size':
+            result.push(stack.length);
+            break;
+
+        case 'empty':
+            result.push(stack[0] ? 0 : 1);
+            break;
+
+        default:
+            stack.push(Number(input[i].split(' ')[1]));
+            break;
+    }
+}
+console.log(result.join('\n'));
