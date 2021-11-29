78f907c3da1d94b073c964bbb66d91fa0a26171a
안서진 <ser0805@gmail.com>
2021-11-29 22:53:18 +0900

    Add BaekJoon: 알고리즘기초1 다이나믹프로그래밍1 11726_2xn타일링

---
+++ baekjoon/알고리즘기초1/다이나믹프로그래밍1/11726_2xn타일링.js
@@ -0,0 +1,14 @@
+let fs = require('fs');
+let input = fs.readFileSync('../../test.txt').toString().split('\n');
+
+const n = Number(input[0]);
+
+let DP = new Array(n+1).fill(0);
+DP[1] = 1;
+DP[2] = 2;
+
+for (let i = 3; i <= n; i++) {
+    DP[i] = (DP[i-1] + DP[i-2])%10007;
+}
+
+console.log(DP[n]);
