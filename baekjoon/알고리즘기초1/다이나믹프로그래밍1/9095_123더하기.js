f91164fe4a1d3d627c5d54d734cde5e1cf2ec69b
안서진 <ser0805@gmail.com>
2021-12-02 01:54:50 +0900

    Refactor: 알고리즘기초1 다이나믹프로그래밍1 9095_123더하기 주석제거

--- baekjoon/알고리즘기초1/다이나믹프로그래밍1/9095_123더하기.js
+++ baekjoon/알고리즘기초1/다이나믹프로그래밍1/9095_123더하기.js
@@ -1,8 +1,6 @@
 let fs = require('fs');
 let input = fs.readFileSync('../../test.txt').toString().split('\n');
 
-// 3 = 1 + 1 + 1, 1 + 2, 2 + 1, 3
-// 4 = 1 + 1 + 1 + 1, 1 + 2 + 1, 2 + 1 + 1, 3 + 1, 1 + 1 + 2, 2 + 2, 1 + 3
 const T = Number(input.shift());
 let result = [];
 let arr = [0, 1, 2, 4];
