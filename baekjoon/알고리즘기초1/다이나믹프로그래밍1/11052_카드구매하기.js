ce939b89ee666b3a383c84d52f331e3f11f9e3da
안서진 <ser0805@gmail.com>
2021-12-04 15:09:21 +0900

    Add BaekJoon: 알고리즘기초1 다이나믹프로그래밍1 11052_카드구매하기

---
+++ baekjoon/알고리즘기초1/다이나믹프로그래밍1/11052_카드구매하기.js
@@ -0,0 +1,26 @@
+// 예를 들어 DP[8]을 구하고자 한다면
+// 8장의 카드가 들어있는 카드팩을 사는 비용 prices[8]
+// 7장의 카드를 사는 최대 비용 + 1장의 카드를 사는 최대 비용 : DP[7] + DP[1]
+// 6장의 카드를 사는 최대 비용 + 2장의 카드를 사는 최대 비용 : DP[6] + DP[2]
+// 5장의 카드를 사는 최대 비용 + 3장의 카드를 사는 최대 비용 : DP[5] + DP[3]
+// 4장의 카드를 사는 최대 비용 * 2 : DP[4] + DP[4]
+
+let fs = require('fs');
+let input = fs.readFileSync('../../test.txt').toString().split('\n');
+
+const N = Number(input.shift());
+const prices = [0].concat(input[0].split(' ').map((el) => parseInt(el, 10)));
+const DP = Array(N + 1).fill(0);
+DP[1] = prices[1]; // 1개를 사는데 최대금액
+
+for (let i = 2; i <= N; i++) {
+    let max = 0;
+    for (let j = 1; j < parseInt(i / 2, 10) + 1; j++) {
+        if (DP[j] + DP[i - j] > max) {
+            max = DP[j] + DP[i - j];
+        }
+    }
+    DP[i] = Math.max(prices[i], max);
+}
+
+console.log(DP[N]); // N개의 카드를 구매하는 최대 비용
