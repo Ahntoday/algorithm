21dcd7a0cf090924f729f1fd8efb0d8481f6103a
안서진 <ser0805@gmail.com>
2021-12-09 23:21:11 +0900

    Add BaekJoon: 알고리즘기초1 다이나믹프로그래밍1 1309_동물원

---
+++ baekjoon/알고리즘기초1/다이나믹프로그래밍1/1309_동물원.js
@@ -0,0 +1,23 @@
+let fs = require('fs');
+let input = fs.readFileSync('../../test.txt').toString().split('\n');
+
+// 1. 사자가 없다.
+// 2. 왼쪽에 사자가 있다.
+// 3. 오른쪽에 사자가 있다.
+
+// dp[i][0] = dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]
+// dp[i][1] = dp[i - 1][0] + dp[i - 1][2]
+// dp[i][2] = dp[i - 1][0] + dp[i - 1][1]
+const N = Number(input.shift());
+const DP = Array.from(Array(100001), () => Array(3));
+DP[1][0] = 1;
+DP[1][1] = 1;
+DP[1][2] = 1;
+
+for (let i = 2; i <= N; i++) {
+    DP[i][0] = (DP[i - 1][0] + DP[i - 1][1] + DP[i - 1][2]) % 9901;
+    DP[i][1] = (DP[i - 1][0] + DP[i - 1][2]) % 9901;
+    DP[i][2] = (DP[i - 1][0] + DP[i - 1][1]) % 9901;
+}
+
+console.log((DP[N][0] + DP[N][1] + DP[N][2]) % 9901);
